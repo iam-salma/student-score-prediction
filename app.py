fc1f43ae58e2c58f9eed955ba9f5e12b4cc3016c
iam-salma <149823820+iam-salma@users.noreply.github.com>
2025-06-04 23:59:41 +0530

    setting port to 3000

--- app.py
+++ app.py
@@ -36,4 +36,4 @@ def predict_datapoint():
 
 if __name__=="__main__":
     #app.run(debug=True, host="0.0.0.0", port=8080)
-    app.run(host="0.0.0.0", port=8080)
+    app.run(host="0.0.0.0", port=3000)
