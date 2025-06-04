a7cce1df8ba5aecef5570793dbef21ee0f3a8c8c
iam-salma <149823820+iam-salma@users.noreply.github.com>
2025-06-04 22:14:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -36,5 +36,5 @@ def predict_datapoint():
 
 
 if __name__=="__main__":
-    #app.run(debug=True, host="0.0.0.0", port=5000)
+    #app.run(debug=True, host="0.0.0.0", port=8080)
     app.run(host="0.0.0.0", port=8080)
